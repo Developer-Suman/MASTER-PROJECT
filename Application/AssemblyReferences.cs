2d25f852fc53f34920cbdd0b8f7e8165e19f72c0
Suman Rai <officeproj111@gmail.com>
2024-01-02 02:03:23 +0545

    Issue-5 Configure assembly References and Program class

--- Application/AssemblyReferences.cs
+++ Application/AssemblyReferences.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Application
 {
-    public class AssemblyReferences
+    public static class AssemblyReferences
     {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(x=>x.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            return services;
+        }
     }
 }
---
+++ Application/Authentication/IJwtProvider.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Authentication
+{
+    public interface IJwtProvider
+    {
+        string Genetrate(ApplicationUser user, IList<string> roles );
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+    }
+}
